d894dd8440f1002bde960cf7014e4514ba5d7a11
amaruki <krilinamar@gmail.com>
2025-11-13 20:55:29 +0700

    refactor(routing): move events routes to public route group

---
+++ src/app/(public)/layout.tsx
@@ -0,0 +1,17 @@
+/**
+ * Layout for the events section
+ */
+
+import { ReactNode } from 'react';
+
+interface SiteProps {
+  children: ReactNode;
+}
+
+export default function Site({ children }: SiteProps) {
+  return (
+    <>
+        {children}
+    </>
+  );
+}
